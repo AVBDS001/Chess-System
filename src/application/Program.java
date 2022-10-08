efea568be629c649502ae909615c7a4f5f98fabd
AVBDS001 <avbds2077@outlook.com>
2022-10-08 20:04:10 -0300

    Check logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Program {
-    //REFAZER A AULA 193 DO PROJETO DE XADREZ
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
